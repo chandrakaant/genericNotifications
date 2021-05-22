f3a5173fbdf8c862790a4a9a4db8497d07c31d83
Chandrakant Patel <chandrakant.patel@highpeaksw.com>
2021-05-22 20:35:05 +0530

    Added comments

--- src/main/java/com/notifications/genericNotifications/service/OrderStateUpdateService.java
+++ src/main/java/com/notifications/genericNotifications/service/OrderStateUpdateService.java
@@ -3,6 +3,7 @@ package com.notifications.genericNotifications.service;
 import com.notifications.genericNotifications.Bean.EmailBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 import static com.notifications.genericNotifications.Utils.StringConstants.*;
@@ -17,18 +18,25 @@ public class OrderStateUpdateService {
     private String fromEmail;
 
 
+    /**
+     * Function to send predefined mail to recepient
+     * @return String confirmation of state change
+     * @throws Exception generic exception
+     */
     public String updateStateAndNotifyUser() throws Exception {
         // Business Logic to update or change state to trigger notification
 
-        /**
-         * send email notification with predefined details
-         * toAddress must be changed for each user.
-         */
         notificationService.sendMimeMessageEmail
                 ("notification@sharklasers.com", STATE_UPDATE_EMAIL_SUBJECT, STATE_UPDATE_EMAIL_GREETINGS + "user name" + STATE_UPDATE_EMAIL_BODY, fromEmail);
         return "Updated state";
     }
 
+    /**
+     * Function to send custom email to recepient
+      * @param emailBean request body containing custom email body
+     * @return String confirmation of state change
+     * @throws Exception generic exception
+     */
     public String updateStateAndNotifyUser(EmailBean emailBean) throws Exception {
         // Business Logic to update or change state to trigger notification
 
@@ -41,8 +49,4 @@ public class OrderStateUpdateService {
         return "Updated state";
     }
 
-    public String updateStateAndNotifyUserOnSlack() {
-
-        return "Update State";
-    }
 }
